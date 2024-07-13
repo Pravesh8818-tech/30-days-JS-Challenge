fa29cfd7ea852c5376d5baca919ba78cd5c492bd
pravesh yadav <praveshtech109@gmail.com>
2024-07-14 00:20:31 +0530

    day1 challenge

---
+++ Day-1 Variable and Datatype/solution.js
@@ -0,0 +1,48 @@
+// Task 1
+var age = 25;
+console.log(age);
+
+//******* Task 2 */
+let str = 'This is a string';
+console.log(str);
+
+//***** Task 3 */
+const result = true;
+console.log(result);
+
+//***** Task 4 */
+// #Datatypes
+let num = 10; // Number
+console.log(typeof num);
+
+const cname = 'Goldi Shukla'; // String
+console.log(typeof cname);
+
+const flag = true; // Boolean
+console.log(typeof flag);
+
+// Object
+let user = {
+  cname: 'Goldi Sharma',
+  age: '21',
+  qualification: 'MCA',
+};
+console.log(typeof user);
+
+// Array
+let arr = [10, 20, 30, 40];
+console.log(typeof arr);
+
+//****** Task 5 */
+let username = 'Pravesh yadav';
+console.log(username);
+username = 'Shobhit Shukla';
+console.log(username);
+
+//***** Task 6 */
+const lib = 'React JS';
+console.log(lib);
+// reassigning a const variable
+lib = 'Three JS';
+console.log(lib);
+// TypeError: Assignment to constant variable.
